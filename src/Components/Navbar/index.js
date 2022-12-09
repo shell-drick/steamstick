7e95b8bec1e0273095fe050ec3cb345c0faaf2ba
Andrew Sheldrick <ajsheld12@gmail.com>
2022-12-09 00:46:43 -0500

    cleaning up git BS

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from 'react';
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements';
-
-
-const Navbar = () => {
-
-    return(
-        <>
-            <Nav >
-                <NavbarContainer>
-                    <NavLogo to='/' >
-                        SteamStick
-                    </NavLogo>
-                    <MobileIcon >
-                        
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to="Summary" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Summary</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="Bio" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Bio</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="ServerDeploy" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Server Deploy</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="Refrences" smooth={true} duration={500} spy={true} exact='true' offset={-80}>Refrences</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
-        </>
-    );
-};
-
-=======
 import React, {useState, useEffect} from 'react';
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements';
 
@@ -75,5 +37,4 @@ const Navbar = () => {
     );
 };
 
->>>>>>> af85549bd44c765ced4c669df58fc242424297b2
 export default Navbar
